1ff1e3b8f77ca5caf1cd1b3a6f9327a6b0d8e9b0
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2021-07-28 13:37:35 -0400

    Address checkstyle issues in test directory
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/test/java/org/mskcc/cmo/metadb/config/MockDataConfig.java
+++ src/test/java/org/mskcc/cmo/metadb/config/MockDataConfig.java
@@ -1,18 +1,25 @@
 package org.mskcc.cmo.metadb.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.mockito.Mockito;
 import org.mskcc.cmo.common.FileUtil;
 import org.mskcc.cmo.metadb.model.MockJsonTestData;
+import org.mskcc.cmo.metadb.service.impl.ValidRequestCheckerImpl;
+import org.mskcc.cmo.metadb.service.util.RequestStatusLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.mskcc.cmo.metadb.service.impl.ValidRequestCheckerImpl;
-import org.mskcc.cmo.metadb.service.util.RequestStatusLogger;
 
 @Configuration
 @ComponentScan(basePackages = "org.mskcc.cmo.common.*")
@@ -22,6 +29,7 @@ public class MockDataConfig {
     private final String MOCKED_JSON_DATA_DIR = "data";
 
     private ClassPathResource mockJsonTestDataResource;
+
     @Autowired
     private void setMockJsonTestDataResource() {
         this.mockJsonTestDataResource = new ClassPathResource(MOCKED_JSON_DATA_DIR);
@@ -36,20 +44,27 @@ public class MockDataConfig {
     }
 
     @Bean
-    public ValidRequestCheckerImpl ValidRequestCheckerImpl() {
+    public ValidRequestCheckerImpl validRequestCheckerImpl() {
         return new ValidRequestCheckerImpl();
     }
-    
+
     @Bean
-    public RequestStatusLogger RequestStatusLogger() {
+    public RequestStatusLogger requestStatusLogger() {
         return Mockito.mock(RequestStatusLogger.class);
     }
 
     private Map<String, MockJsonTestData> mockedRequestJsonDataMap;
+
+    /**
+     * Generates the mocked request json data map;
+     * @return
+     * @throws IOException
+     */
     @Bean(name = "mockedRequestJsonDataMap")
     public Map<String, MockJsonTestData> mockedRequestJsonDataMap() throws IOException {
         this.mockedRequestJsonDataMap = new HashMap<>();
-        ClassPathResource jsonDataDetailsResource = new ClassPathResource(MOCKED_REQUEST_DATA_DETAILS_FILEPATH);
+        ClassPathResource jsonDataDetailsResource =
+                new ClassPathResource(MOCKED_REQUEST_DATA_DETAILS_FILEPATH);
         BufferedReader reader = new BufferedReader(new FileReader(jsonDataDetailsResource.getFile()));
         List<String> columns = new ArrayList<>();
         String line;
@@ -62,19 +77,22 @@ public class MockDataConfig {
             String identifier = data[columns.indexOf("identifier")];
             String filepath = data[columns.indexOf("filepath")];
             String description = data[columns.indexOf("description")];
-            mockedRequestJsonDataMap.put(identifier, createMockJsonTestData(identifier, filepath, description));
+            mockedRequestJsonDataMap.put(identifier,
+                    createMockJsonTestData(identifier, filepath, description));
         }
         reader.close();
         return mockedRequestJsonDataMap;
     }
 
-    private MockJsonTestData createMockJsonTestData(String identifier, String filepath, String description) throws IOException {
+    private MockJsonTestData createMockJsonTestData(String identifier, String filepath,
+            String description) throws IOException {
         String jsonString = loadMockRequestJsonTestData(filepath);
         return new MockJsonTestData(identifier, filepath, description, jsonString);
     }
 
     private String loadMockRequestJsonTestData(String filepath) throws IOException {
-        ClassPathResource res = new ClassPathResource(mockJsonTestDataResource.getPath() + File.separator + filepath);
+        ClassPathResource res = new ClassPathResource(mockJsonTestDataResource.getPath()
+                + File.separator + filepath);
         Map<String, Object> filedata = mapper.readValue(res.getFile(), Map.class);
         return mapper.writeValueAsString(filedata);
     }
