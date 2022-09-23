f658aa8b39f4f2b172a0632dc50b35e64f281def
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2022-09-23 14:31:29 -0400

    Hotfix
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/main/java/org/mskcc/smile/service/impl/ValidateUpdatesMsgHandlingServiceImpl.java
+++ src/main/java/org/mskcc/smile/service/impl/ValidateUpdatesMsgHandlingServiceImpl.java
@@ -36,7 +36,7 @@ public class ValidateUpdatesMsgHandlingServiceImpl implements ValidateUpdatesMes
     @Value("${smile.request_update_topic}")
     private String SERVER_REQUEST_UPDATE_TOPIC;
 
-    @Value("${smile.sample_update_topic")
+    @Value("${smile.sample_update_topic}")
     private String SERVER_SAMPLE_UPDATE_TOPIC;
 
     @Value("${num.new_request_handler_threads}")
