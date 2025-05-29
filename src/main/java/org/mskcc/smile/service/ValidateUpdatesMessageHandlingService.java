607827a705d2bb51d895aabbaae30789f2d5fcf1
ao508 <15623749+ao508@users.noreply.github.com>
2025-05-29 13:11:35 -0700

    Sample updates are handled as an array (#79) (#80)
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/main/java/org/mskcc/smile/service/ValidateUpdatesMessageHandlingService.java
+++ src/main/java/org/mskcc/smile/service/ValidateUpdatesMessageHandlingService.java
@@ -1,10 +1,11 @@
 package org.mskcc.smile.service;
 
+import java.util.List;
 import org.mskcc.cmo.messaging.Gateway;
 
 public interface ValidateUpdatesMessageHandlingService {
     void initialize(Gateway gateway) throws Exception;
     void requestUpdateFilterHandler(String requestJson) throws Exception;
-    void sampleUpdateFilterHandler(String sampleJson) throws Exception;
+    void sampleUpdateFilterHandler(List<Object> sampleJsonList) throws Exception;
     void shutdown() throws Exception;
 }
